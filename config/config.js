e14a49d8571d88fda18ed36f99ef7355a756f3d0
eRichards11 <107233205+eRichards11@users.noreply.github.com>
2022-08-10 11:11:59 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoDbUrl: `mongodb+srv://test1:testing123@atlascluster.vqftcve.mongodb.net/userDR?retryWrites=true&w=majority`,
+    mongoDbUrl: `mongodb+srv://####:#####@atlascluster.vqftcve.mongodb.net/#####?retryWrites=true&w=majority`,
     PORT: process.env.PORT || 3000,
     globalVariables: (req, res, next) => {
         res.locals.success_message = req.flash('success-message')
